a2e07a128ffadd76b1c8a29a2f8bf0b870e76f4d
GabSeven <gabrodrigomes@gmail.com>
2025-07-31 16:43:35 -0300

    cabo iEEE

--- IEEE_representacao/IEEE_representacao.c
+++ IEEE_representacao/IEEE_representacao.c
@@ -4,6 +4,10 @@
 #define Tdouble 0
 #define Tfloat 1
 
+#define BF 127
+#define BD 1023
+#define sqrt2 1.4142135623730950488016887
+
 // typedef union dIEEE {
 //     double x;
 //     struct{
@@ -30,33 +34,64 @@ typedef struct{
     void *data;
 }real;
 
+
+float fIEEE_f(fIEEE x) {
+    unsigned long int bits = 0;
+    bits |= (x.s & 0x1) << 31;
+    bits |= (x.E & 0xFF) << 23;
+    bits |= (x.f & 0x7FFFFF);
+    float f;
+    *((unsigned long int *)&f) = bits;
+    return f;
+}
+
+double dIEEE_d(dIEEE x) {
+    unsigned long long int bits = 0;
+    bits |= ((unsigned long long int)(x.s & 0x1)) << 63;
+    bits |= ((unsigned long long int)(x.E & 0x7FF)) << 52;
+    bits |= (x.f & 0xFFFFFFFFFFFFF);
+    double d;
+    *((unsigned long long int *)&d) = bits;
+    return d;
+}
+
+fIEEE f_fIEEE(float f) {
+    unsigned long int bits = *((unsigned long int *)&f);
+    fIEEE ieee;
+    ieee.s = (bits >> 31) & 0x1;
+    ieee.E = (bits >> 23) & 0xFF;
+    ieee.f = bits & 0x7FFFFF;
+    return ieee;
+}
+
+dIEEE d_dIEEE(double d) {
+    unsigned long long int bits = *((unsigned long long int *)&d);
+    dIEEE ieee;
+    ieee.s = (bits >> 63) & 0x1;
+    ieee.E = (bits >> 52) & 0x7FF;
+    ieee.f = bits & 0xFFFFFFFFFFFFF;
+    return ieee;
+}
+
+
 real novo_real(double x, int t){
     real r;
     r.type = t;
     if(t==Tdouble){
         dIEEE *pt_data;
         pt_data = (dIEEE *) malloc(sizeof(dIEEE));
-        
-        unsigned long long int *px = (unsigned long long int *)&x;
-        pt_data->f = (*px) & 0xFFFFFFFFFFFFF; // 52
-        pt_data->E = ((*px) >> 52) & 0x7FF;  // 0b111 1111 1111 => 11
-        pt_data->s = ((*px) >> (52 + 11)) & 0x1;
+        *pt_data = d_dIEEE(x);
         r.data = (void *) pt_data;
     }else if(t==Tfloat){
 
         fIEEE *pt_data;
         pt_data = (fIEEE *) malloc(sizeof(fIEEE));
-
-        float y = x;
-        
-        unsigned long int *px = (unsigned long int *)&y;
-        pt_data->f = (*px) & 0x7FFFFF; // 23
-        pt_data->E = ((*px) >> 23) & 0xFF; // 0b1111 1111 => 8
-        pt_data->s = ((*px) >> (23 + 8)) & 0x1;
+        *pt_data = f_fIEEE((float) x);
         r.data = (void *) pt_data;
     }
     return r;
 };
+
 
 void exclui_real(real *x){
     if(x->data!=NULL){
@@ -78,58 +113,7 @@ A função deve analisar se o valor A é representado como Tfloat ou Tdouble.
 Se o tipo não for Tdouble nem Tfloat, o comportamento é indefinido (ou seja, não se sabe o que será retornado) 
 */
 
-real sqrtNR(real A){
-    if (A.type ==  Tfloat) {
-        printf("bbb\n");
-        if (*(float *) A.data == 0 || *(float *) A.data == 1) {
-            return A;
-        } else {
-            // *(float *) A.data = 2;
-        
-            const int B = 127; //valor do bias para float
-            const double sqrt2 =  1.4142135623730950488016887;
-            int k = 0;
 
-            fIEEE x0 , x1;
-            
-            x0 = *(fIEEE*) A.data;
-            int impar = !(x0.E & 1); /// expoente verdadeiro � �mpar???
-            x0.E = ((x0.E>>1) + 0x3F ); /// cuidado com o double
-            x0.f >>= 1; /// metade da fra��o da mantissa
-            if (impar) {
-                // x0 = (fIEEE)  (((float) x0) * sqrt2) ;
-            }
-
-            // return * ((float*) A.data);
-        //     printf("\n  E  = %d ", x0.bits.E);
-        //     printf("\n  f  = %d ", x0.bits.f);
-        //     printf("\n  f  = %f ", (float)x0.bits.f/(1<<23));
-        //     printf("\n  s  = %d ", x0.bits.s);
-
-
-        //     printf("\n  Estimativa Inicial x0.x  = %f  ", x0.x );
-
-        //     x1.x = x0.x;
-
-        //     do { /// newton-rapson   /// dessa maneira, se X.x0 == 0.0
-        //                             /// temos problemas em usar while-do
-        //             x0.x = x1.x; /// estimativa atual
-        //             x1.x = (x0.x + A/x0.x); /// pr�xima estimativa
-        // /// resolver a multiplica��o por (1/2)
-        //             x1.bits.E--; /// x1.bits.E = x1.bits.E � 1;
-        //             k++;  /// contador de itera��es
-        //     } while (x0.x != x1.x);
-        //     printf("\n  Total de iteracoes  = %d ", k );
-        //     return x1.x;'
-        
-        }
-                
-    } else if (A.type == Tdouble) {
-        printf("");
-    }
-    
-    return A; // Cuidado com esse retorno, pois A.data é uma região de memória que não faz parte da struct
-}
 
 void change_to_Tdouble(real *A){
 /*
@@ -189,6 +173,105 @@ A função real_to_float retorna um valor float a partir de um real;
     return * ((float*) A.data);
 }
 
+real sqrtNR(real A){
+    if (A.type ==  Tfloat) {
+        if (fIEEE_f(*(fIEEE *)A.data ) == 0 || fIEEE_f(*(fIEEE *)A.data )== 1) {
+            return A;
+        } else {
+            int k = 0;
+            
+            fIEEE *x0, *x1;
+            
+            x0 = (fIEEE *) malloc(sizeof(fIEEE));
+            
+            *x0 = *((fIEEE*) A.data); // copia o valor e nao o ponteiro
+            
+            int impar = !(x0->E & 1); /// expoente verdadeiro é ímpar???
+            x0->E = ((x0->E>>1) + 0x3F ); /// cuidado com o doubleetade da fração da mantissa
+            if (impar) {
+                float x =  fIEEE_f(*x0);
+                x *= sqrt2;
+                *x0 = f_fIEEE(x);
+            }
+
+
+            printf("\n  E  = %d ", x0->E - BF);
+            printf("\n  f  = %d ", x0->f);
+            printf("\n  f  = %f ", (float)x0->f/(1<<23));
+            printf("\n  s  = %d ", x0->s);
+
+
+            printf("\n  Estimativa Inicial x0  = %f  ", *(float *)x0 );
+
+            x1 = (fIEEE *) malloc(sizeof(fIEEE));
+            
+            *x1 = *x0;
+
+            do { 
+                *x0 = *x1;
+                *x1 = f_fIEEE(fIEEE_f(*x0) +fIEEE_f(*(fIEEE *)A.data)/fIEEE_f(*x0)); 
+                x1->E--; 
+                k++;
+                    
+            } while ((*(unsigned long int *)x0 ) != (*(unsigned long int *)x1 ) );
+
+            printf("\n  Total de iteracoes  = %d ", k );
+            
+            A.data = (fIEEE *) x0;
+        }
+                
+    } else if (A.type == Tdouble) {
+        if (dIEEE_d(*(dIEEE *)A.data ) == 0 || dIEEE_d(*(dIEEE *)A.data) == 1) {
+            printf("aa");
+            return A;
+        } else {
+            int k = 0;
+            
+            dIEEE *x0, *x1;
+            
+            x0 = (dIEEE *) malloc(sizeof(dIEEE));
+            
+            *x0 = *((dIEEE*) A.data); 
+            
+            int impar = !(x0->E & 1); 
+            x0->E = ((x0->E>>1) + 0x1FF ); 
+            x0->f >>= 1;
+            if (impar) {
+                double x =  dIEEE_d(*x0);
+                x *= sqrt2;
+                *x0 = d_dIEEE(x);
+            }
+
+
+            printf("\n  E  = %d ", x0->E - BD);
+            printf("\n  f  = %d ", x0->f);
+            printf("\n  f  = %f ", (double)x0->f/(1<<52));
+            printf("\n  s  = %d ", x0->s);
+
+
+            printf("\n  Estimativa Inicial x0  = %f  ", *(double *)x0 );
+
+            x1 = (dIEEE *) malloc(sizeof(dIEEE));
+            
+            *x1 = *x0;
+
+            do { 
+                *x0 = *x1;
+                *x1 = d_dIEEE(dIEEE_d(*x0) +dIEEE_d(*(dIEEE *)A.data)/dIEEE_d(*x0)); 
+                x1->E--; 
+                k++;
+                    
+            } while ((*(unsigned long long int *)x0 ) != (*(unsigned long long int *)x1 ) );
+            
+            printf("\n  Total de iteracoes  = %d ", k );
+            
+            A.data = (dIEEE *) x0;
+        }
+    }
+    
+    return A; // Cuidado com esse retorno, pois A.data é uma região de memória que não faz parte da struct
+}
+
 void printBinary(int *num, int tam) {
     for (int i = sizeof(int) * tam / 4 - 1; i >= 0; i--) {
         printf("%d", (*num >> i) & 1);
@@ -197,7 +280,7 @@ void printBinary(int *num, int tam) {
 
 int main()
 {
-    float x = 3;
+    float x = 4;
     // 01010
     real r1, r2;
     r1 = novo_real(x, Tfloat);
@@ -205,18 +288,18 @@ int main()
     
     fIEEE *as_float;
     dIEEE *as_double;
-    
     real sqrtR1 = sqrtNR(r1); // essa chamada da função deve retornar um valor real com type == Tfloat, pois r1.type == Tfloat 
-    // real sqrtR2 = sqrtNR(r2); // essa chamada da função deve retornar um valor real com type == Tdouble, pois r2.type == Tdouble
+    real sqrtR2 = sqrtNR(r2); // essa chamada da função deve retornar um valor real com type == Tdouble, pois r2.type == Tdouble
     
     float sqrt_xF =      real_to_float(sqrtR1);
     // float sqrt_xD_to_F = real_to_float(sqrtR2);
-    // double sqrt_xD =     real_to_double(sqrtR2);
+    double sqrt_xD =     real_to_double(sqrtR2);
     // double sqrt_xF_to_D =real_to_double(sqrtR1);
 
-    printf("f to f sqrt(%lf):%f\n",x,sqrt_xF);
+    
+    printf("\nf sqrt(%f):%f\n",x,sqrt_xF);
     // printf("d to f sqrt(%lf):%f\n",x,sqrt_xD_to_F);
-    // printf("d to d sqrt(%lf):%lf\n",x,sqrt_xD);
+    printf("d sqrt(%lf):%lf\n",x,sqrt_xD);
     // printf("f to d sqrt(%lf):%lf\n",x,sqrt_xF_to_D);
     
     as_float = r1.data;
@@ -224,26 +307,12 @@ int main()
     
     printf("sinal:%lx mantissa:%lx expoente:%lx\n", (long int) as_float->s, (long int) as_float->f, (long int) as_float->E - 127);
     printf("sinal:%llx mantissa:%llx expoente:%llx\n", (long long int) as_double->s,  (long long int)as_double->f,  (long long int)as_double->E - 1023);
+
     float f = real_to_float(r1);
-    printf("teste: %fz", f);
+    printf("float: %fz", f);
 
     double d = real_to_double(r2);
     printf("double: %lf\n", d); 
-
-    // printf("r1: ");
-    // printBinary(r1.data, 32);    
-
-    // printf("r2:");
-    // printBinary(r2.data, 64);
-    // printf("mantissa:");
-    // printBinary(as_float->bits.f, 23);
-    
-    // printf("ok: %llx\n", r1.data);
-    
-    // printf("sinal:%x mantissa:%x expoente:%x\n",as_float->s, as_float->f, as_float->E);
-    // printf("sinal:%llx mantissa:%llx expoente:%llx\n",as_double->s, as_double->f, as_double->E);
-    // exclui_real(&r1);
-    // exclui_real(&r2);
 
     return 0;
 }
